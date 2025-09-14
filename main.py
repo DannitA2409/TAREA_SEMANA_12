b40c50982613eae2121b3e4ff3401ef28101ef0c
DannitA2409 <dm.lopezl@uea.edu.ec>
2025-09-14 05:26:02 -0500

    agregar semanas
    El código fue modificado para pasar de 2 semanas a 4

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 ciudades = ["Quito", "Guayaquil", "Cuenca"]
 dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-semanas = 2
+semanas = 4
 
 # matriz 3D (ciudad x semana x día)
 temperaturas = [
-    [   # Ciudad 1: Quito
+    [   # Quito
         [20, 22, 21, 19, 18, 20, 21],   # Semana 1
-        [23, 22, 20, 21, 19, 20, 22]    # Semana 2
+        [23, 22, 20, 21, 19, 20, 22],   # Semana 2
+        [21, 20, 19, 22, 23, 21, 20],   # Semana 3
+        [22, 21, 20, 21, 20, 22, 23]    # Semana 4
     ],
-    [   # Ciudad 2: Guayaquil
+    [   # Guayaquil
         [28, 30, 29, 31, 32, 30, 29],   # Semana 1
-        [30, 31, 32, 33, 31, 29, 30]    # Semana 2
+        [30, 31, 32, 33, 31, 29, 30],   # Semana 2
+        [29, 30, 31, 32, 30, 31, 30],   # Semana 3
+        [31, 32, 33, 32, 31, 30, 29]    # Semana 4
     ],
-    [   # Ciudad 3: Cuenca
+    [   # Cuenca
         [15, 16, 14, 15, 16, 15, 14],   # Semana 1
-        [17, 16, 15, 16, 15, 16, 17]    # Semana 2
+        [17, 16, 15, 16, 15, 16, 17],   # Semana 2
+        [16, 15, 15, 14, 15, 16, 15],   # Semana 3
+        [17, 16, 15, 17, 16, 15, 16]    # Semana 4
     ]
 ]
 
