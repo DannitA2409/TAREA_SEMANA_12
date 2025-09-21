8bda6639ea3db029aceee488cab26f820469d689
DannitA2409 <dm.lopezl@uea.edu.ec>
2025-09-20 21:31:12 -0500

    función calcular_descuento
    Se ha creado la función encargada de calcular el descuento

---
+++ semana_14.py
@@ -0,0 +1,4 @@
+# Definición de la función
+def calcular_descuento(monto_total, porcentaje_descuento=10):
+    descuento = monto_total * (porcentaje_descuento / 100) # Cálculo de valor de descuento
+    return descuento
