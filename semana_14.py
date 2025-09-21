7329d2a16fa53267bb7945e6d738f5edc7775989
DannitA2409 <dm.lopezl@uea.edu.ec>
2025-09-20 21:34:01 -0500

    primera llamada a la función
    Primera llamada a la función calcular_descuento(), solo con el monto y aplicando el descuento por defecto del 10%

--- semana_14.py
+++ semana_14.py
@@ -2,3 +2,17 @@
 def calcular_descuento(monto_total, porcentaje_descuento=10):
     descuento = monto_total * (porcentaje_descuento / 100) # Cálculo de valor de descuento
     return descuento
+
+
+# Primera llamada a la función
+# Solo con el monto (se aplica el descuento por defecto del 10%)
+monto1 = 100
+descuento1 = calcular_descuento(monto1)
+monto_final1 = monto1 - descuento1
+
+print("Primera compra:")
+print("Monto total: $", monto1)
+print("Descuento aplicado: $", descuento1)
+print("Monto final a pagar: $", monto_final1)
+print("---------------")
+
