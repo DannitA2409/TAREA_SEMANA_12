6ccb1c9afdd71d422f785e0567745a8d35fed6bb
DannitA2409 <dm.lopezl@uea.edu.ec>
2025-09-28 01:58:55 -0500

    Crea un diccionario
    Creación de un diccionario con información personal ficticia

---
+++ semana_15.py
@@ -0,0 +1,12 @@
+# Tarea semana 15: Trabajando con Diccionarios en Python
+
+# Crear diccionario con información personal ficticia
+informacion_personal = {
+    "nombre": "Danna López",
+    "edad": 21,
+    "ciudad": "Guayaquil"
+}
+print(f"Diccionario inicial: {informacion_personal}")
+print()
+
+
