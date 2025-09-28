4746524a31a7dd3b351948af277c4189a4a8b0a8
DannitA2409 <dm.lopezl@uea.edu.ec>
2025-09-28 02:02:26 -0500

    Acceso y modificacion de valores
    - Accede al valor asociado con la clave "ciudad" y modifícalo con una ciudad diferente.
    - Agrega una nueva clave-valor al diccionario que represente la "profesion" de la persona.

--- semana_15.py
+++ semana_15.py
@@ -9,4 +9,19 @@ informacion_personal = {
 print(f"Diccionario inicial: {informacion_personal}")
 print()
 
+# ACCEDER Y MODIFICAR VALORES
+print("-------- Accediendo y modificando valores ---------")
+
+# Acceder al valor de la clave "ciudad"
+ciudad_actual = informacion_personal["ciudad"]
+print(f"Ciudad actual: {ciudad_actual}")
+
+# Modificar la ciudad
+informacion_personal["ciudad"] = "Quito"
+print(f"Ciudad modificada: {informacion_personal['ciudad']}")
+
+# Agregar nueva clave-valor para "profesion"
+informacion_personal["profesion"] = "Ingeniera en tecnologías de la información"
+print(f"Profesión agregada: {informacion_personal['profesion']}")
+print(f"Diccionario después de modificaciones: {informacion_personal}")
 
