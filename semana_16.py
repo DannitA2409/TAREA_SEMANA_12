58fe5b883604a95d76a976be5758e7d7317b0ba3
DannitA2409 <dm.lopezl@uea.edu.ec>
2025-10-05 19:35:51 -0500

    lectura del archivo
    Se lee el archivo creado utilizando un ciclo while para recorrer el archivo línea por línea

--- semana_16.py
+++ semana_16.py
@@ -1,15 +1,30 @@
 # Tarea seamana 16: Trabajo con Archivos de Texto en Python
 
+# CREACIÓN DEL ARCHIVO
+
 # Crea un archivo llamado "my_notes.txt"
 archivo = open("my_notes.txt", "w")
 
 # Escribe tres líneas de notas
-archivo.write("Nota 1: Hoy aprendí a usar archivos de texto en Python.\n")
-archivo.write("Nota 2: Los modos 'w' y 'r' sirven para escribir y leer.\n")
-archivo.write("Nota 3: Siempre debo cerrar los archivos después de usarlos.\n")
+archivo.write("Hoy aprendí a usar archivos de texto en Python.\n")
+archivo.write("Los modos 'w' y 'r' sirven para escribir y leer.\n")
+archivo.write("Siempre debo cerrar los archivos después de usarlos.\n")
 
 # Cierra el archivo
 archivo.close()
 
+# LECTURA DE ARCHIVO
+
+# Abre el archivo para leer su contenido
+archivo = open("my_notes.txt", "r")
+
+linea = archivo.readline()  # Lee la primera línea
+# Lee linea por línea hasta el final del archivo
+while linea:
+    print(linea.strip())  # Elimina los saltos de línea
+    linea = archivo.readline()
+
+# Cierra el archivo después de leer
+archivo.close()
 
 
